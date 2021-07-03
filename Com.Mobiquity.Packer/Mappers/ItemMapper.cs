3ec3280b6683aabf54b2c0dfb59ca97f2decd905
Serdar Akis <serdarakis@hotmail.com.tr>
2021-07-03 21:54:41 +0300

    Add Object Mappers

---
+++ Com.Mobiquity.Packer/Mappers/ItemMapper.cs
@@ -0,0 +1,29 @@
+﻿using Com.Mobiquity.Packer.Models;
+using System;
+
+namespace Com.Mobiquity.Packer.Mappers
+{
+    static class ItemMapper
+    {
+        public static Item MapToItem(this string data)
+        {
+            var itemInformations = data.Remove(0).Remove(data.Length - 1).Split(',');
+
+            if (int.TryParse(itemInformations[0], out var index))
+                throw new ArgumentException(nameof(index));
+
+            if (float.TryParse(itemInformations[1], out var weight))
+                throw new ArgumentException(nameof(weight));
+
+            if (int.TryParse(itemInformations[2], out var cost))
+                throw new ArgumentException(nameof(cost));
+
+            return new Item(index, weight, cost);
+        }
+
+        public static string MapToString(this Item item)
+        {
+            return "";
+        }
+    }
+}
---
+++ Com.Mobiquity.Packer/Mappers/PackageMapper.cs
@@ -0,0 +1,25 @@
+﻿using Com.Mobiquity.Packer.Models;
+using System;
+using System.Linq;
+
+namespace Com.Mobiquity.Packer.Mappers
+{
+    static class PackageMapper
+    {
+        public static Package MapToPackage(this string data)
+        {
+            var packageInformations = data.Split(':');
+            if (!int.TryParse(packageInformations[0].Trim(), out var packageCapacity))
+                throw new ArgumentException();
+
+            var itemInformations = packageInformations[1].Trim().Split(' ');
+
+            return new Package(packageCapacity, itemInformations.Select(item => item.MapToItem()).ToArray());
+        }
+
+        public static string MapToString(this Package data)
+        {
+            return "";
+        }
+    }
+}
