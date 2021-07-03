3dffe61c853ea684a3ccf77d2e3a0f4c492faaaa
Serdar Akis <serdarakis@hotmail.com.tr>
2021-07-03 23:22:20 +0300

    Add Constraints Class with Constant

---
+++ Com.Mobiquity.Packer/Constants/Constraints.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Mobiquity.Packer.Constants
+{
+    public class Constraints
+    {
+        public const float PackageMaxWeight = 100;
+        public const float ItemMaxWeight = 100;
+        public const int PackageMaxNumberOfItems = 15;
+    }
+
+}
