0d102df51b999f33335b85bb9ebe27713664de22
Serdar Akis <serdarakis@hotmail.com.tr>
2021-07-03 22:38:21 +0300

    Add Comments to Mappers

--- Com.Mobiquity.Packer/Mappers/ItemMapper.cs
+++ Com.Mobiquity.Packer/Mappers/ItemMapper.cs
@@ -5,25 +5,26 @@ namespace Com.Mobiquity.Packer.Mappers
 {
     static class ItemMapper
     {
+        /// <summary>
+        /// Parses Item informations from string. Example input : (6,46.34,€48)
+        /// </summary>
+        /// <param name="data">string => Item informations</param>
+        /// <returns>Item => new instance of Item class</returns>
+        /// <example>(6,46.34,€48)</example>
         public static Item MapToItem(this string data)
         {
-            var itemInformations = data.Remove(0).Remove(data.Length - 1).Split(',');
+            var itemInformations = data.Remove(data.Length - 1).Remove(0, 1).Split(',');
 
-            if (int.TryParse(itemInformations[0], out var index))
+            if (!int.TryParse(itemInformations[0], out var index))
                 throw new ArgumentException(nameof(index));
 
-            if (float.TryParse(itemInformations[1], out var weight))
+            if (!float.TryParse(itemInformations[1], out var weight))
                 throw new ArgumentException(nameof(weight));
 
-            if (int.TryParse(itemInformations[2], out var cost))
+            if (!int.TryParse(itemInformations[2].Remove(0, 1), out var cost))
                 throw new ArgumentException(nameof(cost));
 
             return new Item(index, weight, cost);
-        }
-
-        public static string MapToString(this Item item)
-        {
-            return "";
         }
     }
 }
--- Com.Mobiquity.Packer/Mappers/PackageMapper.cs
+++ Com.Mobiquity.Packer/Mappers/PackageMapper.cs
@@ -6,20 +6,22 @@ namespace Com.Mobiquity.Packer.Mappers
 {
     static class PackageMapper
     {
+        /// <summary>
+        /// Parses Package informations from string. Example input : 81 : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)
+        /// </summary>
+        /// <param name="data">string => Package informations</param>
+        /// <returns>Item => new instance of Package class</returns>
+        /// <example>81 : (1,53.38,€45) (2,88.62,€98) (3,78.48,€3) (4,72.30,€76) (5,30.18,€9) (6,46.34,€48)</example>
         public static Package MapToPackage(this string data)
         {
             var packageInformations = data.Split(':');
             if (!int.TryParse(packageInformations[0].Trim(), out var packageCapacity))
                 throw new ArgumentException();
 
+            //Split string by space gives us item information with parentheses. Item mapper will handle the rest
             var itemInformations = packageInformations[1].Trim().Split(' ');
 
             return new Package(packageCapacity, itemInformations.Select(item => item.MapToItem()).ToArray());
-        }
-
-        public static string MapToString(this Package data)
-        {
-            return "";
         }
     }
 }
