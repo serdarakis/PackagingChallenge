e72bc52235b7fd3112f95d3c6feb93da3709e78e
Serdar Akis <serdarakis@hotmail.com.tr>
2021-07-04 19:01:52 +0300

    Add SampleApp to Test Nuget Package

---
+++ SampleApp/Com.Mobiquity.Packer.SampleApp.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Com.Mobiquity.Packer.SampleApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Packer.Pack("Resources/example_input").Split(Environment.NewLine);
+
+            var fileLines = File.ReadAllLines("Resources/example_output");
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                if(result[i] != fileLines[i])
+                {
+                    Console.WriteLine("Packing Failed");
+                    return;
+                }
+            }
+            
+            Console.WriteLine("Packing Successfull");    
+        }
+    }
+}
